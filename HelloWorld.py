bac74ccace4c7b10c96a2b41a3817d8f338046c9
Abhinav Banerjee <banerjeeabhinav1998@gmail.com>
2024-04-05 01:48:24 +0530

    python script added

--- HelloWorld.py
+++ HelloWorld.py
@@ -6,4 +6,4 @@ number1 = float(input("Enter the first number: "))
 number2 = float(input("Enter the second number: "))
 
 result = add_numbers(number1, number2)
-print(f"The sum of {number1} and {number2} is: {result}")
+print(f"The actual sum of {number1} and {number2} is: {result}")
