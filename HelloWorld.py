10b41886bc8702901200877f06aaad79d085466f
Abhinav Banerjee <banerjeeabhinav1998@gmail.com>
2024-04-05 01:38:36 +0530

    python script added

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,9 @@
-print("Hello, I use Jenkins for CICD!!!")
+def add_numbers(num1, num2):
+    sum_result = num1 + num2
+    return sum_result
+
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+result = add_numbers(number1, number2)
+print(f"The sum of {number1} and {number2} is: {result}")
